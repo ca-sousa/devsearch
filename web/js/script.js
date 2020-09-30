cf76a2369ca12f0bd2b0ea2d177fd6439a192325
Carla Sousa <sousa.carla09@gmail.com>
2020-09-30 14:33:45 -0300

    Events and filter

--- web/js/script.js
+++ web/js/script.js
@@ -12,10 +12,12 @@ let filterName = "";
 let listDevs = null;
 let form = null;
 let devsSearch = null;
+let preLoader = null;
 
 window.addEventListener("load", async () => {
   getValues();
   await fetchDev();
+  events();
 });
 
 function getValues() {
@@ -27,6 +29,7 @@ function getValues() {
   devsSearch = document.querySelector("#devsSearch")
   form = document.querySelector("#searchForm");
   listDevs = document.querySelector("#devs");
+  preLoader = document.querySelector("#preLoader");
 
   checkBoxLang.forEach((language) =>
     language.checked ? filterLanguages.push(language.id) : null
@@ -99,34 +102,97 @@ async function fetchDev() {
       };
     }
   )
-  // renderDevList(devList);
-  console.log(devList)
+  renderDevList(devList);
+  showPreLoader();
+}
+
+// events
+function events() {
+  inputSearch.addEventListener("input", handleKeyUp);
+  checkBoxLang.forEach((checkBoxLang) => {
+    checkBoxLang.addEventListener("input", handleSelectLanguage);
+  });
+  radioLogOperator.forEach((radioLogOperator) => {
+    radioLogOperator.addEventListener("input", handleLogicalOperator);
+  });
+}
+
+function showPreLoader() {
+  setTimeout(() => {
+    preLoader.classList.add("hidden");
+    devsSearch.classList.remove("hidden");
+    activaInput();
+  }, 2000)
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-
-  form.submit(featchDev);
 }
 
-function renderDevList() {
-  let devsHTML = "<div class='devs'>";
-  
-  devList.forEach((dev, index) => {
-    const { name, picture, programmingLanguages } = dev;
-    
-    const devHTML = `
-    <div class="dev">
-    <div class="picture">
-    <img src="${picture}" alt="${name}" />
-    </div>
-    <p>${name}</p>
-                <div>
-                    <p>${programmingLanguages}</p>
-                </div>
-            </div>
-        `;
+function activaInput() {
+  inputSearch.focus();
+}
 
-    devsHTML += devHTML;
+function handleKeyUp(event) {
+  filterName = event.target.value;
+  filterDevs();
+}
+
+function handleSelectLanguage() {
+  filterLanguages = [];
+  const filter = document.querySelectorAll("[name=language]:checked");
+  filter.forEach(({id}) => {
+    filterLanguages.push(id);
   });
+  filterDevs();
+}
+
+function handleLogicalOperator(event) {
+  filterLogOperator = event.target.id;
+  filterDevs();
+}
+
+// filtragem
+function filterDevs() {
+  let filterDevs = [];
+  const filterLowerCase = filterName.toLowerCase();
+  filterDevs = devList.filter((dev) => {
+    return dev.nameTreated.includes(filterLowerCase);
+  });
+  filterDevs = filterDevs.filter((dev) => {
+    return filterLogOperator == "or"
+      ? filterLanguages.some((item) => dev.languageTypes.includes(item))
+      : dev.languageTypes.join("") === filterLanguages.join("");
+  });
+  renderDevList(filterDevs);
+}
+
+// render
+function renderDevList(devs) {
+  listDevs.innerHTML = "";
+  const h3 = document.createElement("h3");
+
+  if (devs === null) {
+    h3.innerHTML = "Não foi encontrado nenhum dev."
+    listDevs.appendChild(h3);
+  } else {
+    h3.innerHTML = `${devs.length} devs encontrados`
+    listDevs.appendChild(h3)
+    const devHTML = ``
+
+    devs.forEach((dev, index) => {
+      const { name, picture, languageTypes, progLanguageIcon } = dev;
+      
+      const HTML = `
+        <div class="dev">
+          <img src="${picture}" alt="${name}" />
+          <p>${name}</p>
+          <img src="${progLanguageIcon}" alt="${languageTypes}">
+        </div>
+      `;
+          devHTML.appendChild(HTML);
+    });
+    listDevs.appendChild(devHTML);
+  }
+
 }
