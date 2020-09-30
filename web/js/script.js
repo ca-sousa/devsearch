a55cf3544f70bd5310f8fd0fa2849c551250496a
Carla Sousa <sousa.carla09@gmail.com>
2020-09-30 12:06:13 -0300

    Treated Resonse

--- web/js/script.js
+++ web/js/script.js
@@ -1,21 +1,106 @@
+// dados
 let devList = [];
+// inputs
+let inputSearch = null;
+let checkBoxLang = null;
+let radioLogOperator = null;
+// filtros
+let filterLogOperator = null;
+let filterLanguages = [];
+let filterName = "";
+// elementos
+let listDevs = null;
+let form = null;
+let devsSearch = null;
 
-window.addEventListener("load", start);
+window.addEventListener("load", async () => {
+  getValues();
+  await fetchDev();
+});
 
-function start() {
-  totalDevs = document.querySelector("#totalSearch");
+function getValues() {
+  inputSearch = document.querySelector("#search");
+  checkBoxLang = document.querySelectorAll("[name='language']");
+  radioLogOperator = document.querySelectorAll("[name='logicalOperators']");
+  filterLogOperator = document.querySelector("[name='logicalOperators']:checked").id;
+  
+  devsSearch = document.querySelector("#devsSearch")
+  form = document.querySelector("#searchForm");
   listDevs = document.querySelector("#devs");
-  form = document.querySelector("form");
 
-  //inputs
-  const name = document.querySelector("#name");
-  const java = document.querySelector("#java");
-  const js = document.querySelector("#javascript");
-  const python = document.querySelector("#python");
-  const and = document.querySelector("#optionAnd");
-  const or = document.querySelector("#optionOr");
+  checkBoxLang.forEach((language) =>
+    language.checked ? filterLanguages.push(language.id) : null
+  );
+}
 
-  form.addEventListener("submit", handleSubmit);
+
+async function fetchDev() {
+  const req = await fetch("http://localhost:3001/devs");
+  const res = await req.json();
+  
+  devList = res.map((dev) => {
+    const { id, name, picture, programmingLanguages } = dev;
+      
+    const nameTreated = name
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f-\x20]/g, "");
+
+    return {
+      id,
+      name,
+      nameTreated,
+      picture,
+      programmingLanguages,
+    };
+  });
+  devList = devList.map(
+    ({ id, name, nameTreated, picture, programmingLanguages }) => {
+      const progLanguageIcon = [];
+      const languageTypes = [];
+
+      programmingLanguages.forEach((languages) => {
+        const { language, id} = languages;
+        const idLowerCase = id.toLowerCase();
+        languageTypes.push(language.toLowerCase());
+        switch (language) {
+          case "Java":
+            progLanguageIcon.push({
+              idLowerCase,
+              language,
+              icon: "./assets/java.png",
+            });
+            break;
+          case "JavaScript":
+            progLanguageIcon.push({
+              idLowerCase,
+              language,
+              icon: "./assets/javascript.png",
+            });
+            break;
+          case "Python":
+            progLanguageIcon.push({
+              idLowerCase,
+              language,
+              icon: "./assets/python.png",
+            });
+            break;
+            default:
+            break;
+        }
+      });
+      return { 
+        id, 
+        name,
+        nameTreated, 
+        picture,
+        languageTypes,
+        progLanguageIcon,
+      };
+    }
+  )
+  // renderDevList(devList);
+  console.log(devList)
 }
 
 function handleSubmit(event) {
@@ -24,34 +109,18 @@ function handleSubmit(event) {
   form.submit(featchDev);
 }
 
-async function featchDev() {
-  const req = await fetch("http://localhost:3001/devs");
-  const res = await req.json();
-
-  devList = res.map((dev) => {
-    const { name, picture, programmingLanguages } = dev;
-
-    return {
-      name,
-      picture,
-      programmingLanguages,
-    };
-  });
-  renderDevList();
-}
-
 function renderDevList() {
   let devsHTML = "<div class='devs'>";
-
+  
   devList.forEach((dev, index) => {
     const { name, picture, programmingLanguages } = dev;
-
+    
     const devHTML = `
-            <div class="dev">
-                <div class="picture">
-                    <img src="${picture}" alt="${name}" />
-                </div>
-                <p>${name}</p>
+    <div class="dev">
+    <div class="picture">
+    <img src="${picture}" alt="${name}" />
+    </div>
+    <p>${name}</p>
                 <div>
                     <p>${programmingLanguages}</p>
                 </div>
